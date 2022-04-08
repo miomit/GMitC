73e4b9df73360a0cd3e3bd22e4b4d71c4b2410dd
Yura Movsesyan <movsesyan-03@inbox.ru>
2022-04-08 20:09:49 +0300

    added Percent

---
+++ Operations/Percent.cs
@@ -0,0 +1,9 @@
+namespace GMitC
+{
+    public class Percent : ICalUno, ICalDuo
+    {
+        public double Calculate(double a) => a / 100;
+        public double Calculate(double a, double b) => (a * b) / 100;
+
+    }
+}
